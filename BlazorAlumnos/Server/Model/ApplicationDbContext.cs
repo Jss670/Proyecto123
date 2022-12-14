6459adf0f1cab45e4514607b915e03deb4fb5c9b
Octavio Garcia <jss670@gmail.com>
2022-12-13 20:04:08 -0700

    initial commit

---
+++ BlazorAlumnos/Server/Model/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using BlazorAlumnos.Server.Model.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorAlumnos.Server.Model
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(
+            DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Alumno> Alumnos { get; set; }
+
+        public DbSet<Materia> Materias { get; set; }
+
+    }
+}
---
+++ BlazorAlumnos/Server/Model/Entities/Materia.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorAlumnos.Server.Model.Entities
+{
+    public class Materia
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        public string Nombre { get; set; }
+
+    }
+}
